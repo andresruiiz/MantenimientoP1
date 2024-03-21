aaaf7afaa2f28ba3ebe6153c096d7ab04b6b5fcc
andresruiiz <andresruiz@uma.es>
2024-03-21 11:31:16 +0100

    Update DoubleLinkedList.java

--- p2/src/main/java/deque/DoubleLinkedList.java
+++ p2/src/main/java/deque/DoubleLinkedList.java
@@ -6,82 +6,74 @@ public class DoubleLinkedList<T> implements DoubleLinkedQueue<T> {
     private LinkedNode<T> last;
     private int size;
 
-    public DoubleLinkedList() {
-        first = null;
-        last = null;
-        size = 0;
-    }
-
     @Override
     public void prepend(T value) {
-        LinkedNode<T> newNode = new LinkedNode<>(value, null, null);
+        LinkedNode<T> newNode = new LinkedNode<>(value, null, first);
         if (isEmpty()) {
-            first = newNode;
             last = newNode;
         } else {
-            newNode.setNext(first);
             first.setPrevious(newNode);
-            first = newNode;
         }
+        first = newNode;
         size++;
     }
 
     @Override
     public void append(T value) {
-        LinkedNode<T> newNode = new LinkedNode<>(value, null, null);
+        LinkedNode<T> newNode = new LinkedNode<>(value, last, null);
         if (isEmpty()) {
             first = newNode;
-            last = newNode;
         } else {
-            newNode.setPrevious(last);
             last.setNext(newNode);
-            last = newNode;
         }
+        last = newNode;
         size++;
     }
 
     @Override
     public void deleteFirst() {
-        if (!isEmpty()) {
-            if (size == 1) {
-                first = null;
-                last = null;
-            } else {
-                first = first.getNext();
-                first.setPrevious(null);
-            }
-            size--;
+        if (isEmpty()) {
+            throw new DoubleLinkedQueueException("Deque is empty");
         }
+        if (size == 1) {
+            first = null;
+            last = null;
+        } else {
+            first = first.getNext();
+            first.setPrevious(null);
+        }
+        size--;
     }
 
     @Override
     public void deleteLast() {
-        if (!isEmpty()) {
-            if (size == 1) {
-                first = null;
-                last = null;
-            } else {
-                last = last.getPrevious();
-                last.setNext(null);
-            }
-            size--;
+        if (isEmpty()) {
+            throw new DoubleLinkedQueueException("Deque is empty");
         }
+        if (size == 1) {
+            first = null;
+            last = null;
+        } else {
+            last = last.getPrevious();
+            last.setNext(null);
+        }
+        size--;
     }
 
     @Override
     public T first() {
-        if (!isEmpty()) {
-            return first.getItem();
+        if (isEmpty()) {
+            throw new DoubleLinkedQueueException("Deque is empty");
         }
-        return null;
+        return first.getItem();
     }
 
     @Override
     public T last() {
-        if (!isEmpty()) {
-            return last.getItem();
+        if (isEmpty()) {
+            throw new DoubleLinkedQueueException("Deque is empty");
         }
-        return null;
+        return last.getItem();
     }
 
     @Override
