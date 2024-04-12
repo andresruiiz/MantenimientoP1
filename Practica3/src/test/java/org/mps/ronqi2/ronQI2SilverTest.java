fecdad8af272f00ffb465a01c07c2d9366bda963
Mel-Nicolo <nicolomelley@uma.es>
2024-04-12 11:14:08 +0200

    .

--- Practica3/src/test/java/org/mps/ronqi2/ronQI2SilverTest.java
+++ Practica3/src/test/java/org/mps/ronqi2/ronQI2SilverTest.java
@@ -93,10 +93,12 @@ public class ronQI2SilverTest {
      * se considera que hay una apnea en proceso. Si hay menos de 5 lecturas también debería realizar la media.
      * /
      
-    /*
-        * Realiza un test para comprobar que si se realizan 5 lecturas con valores por debajo de los umbrales,
-        * no se considera que hay una apnea en proceso.
-        */
+    /* Realiza un primer test para ver que funciona bien independientemente del número de lecturas.
+     * Usa el ParameterizedTest para realizar un número de lecturas previas a calcular si hay apnea o no (por ejemplo 4, 5 y 10 lecturas).
+     * https://junit.org/junit5/docs/current/user-guide/index.html#writing-tests-parameterized-tests
+     */
+    
+     
     @DisplayName("Si se realizan 5 lecturas con valores por debajo de los umbrales, no se considera que hay una apnea en proceso")
     @Test
     public void testEvaluarApneaSuenyo() {
@@ -121,8 +123,33 @@ public class ronQI2SilverTest {
         assertTrue(!result);
     }
 
-     /* Realiza un primer test para ver que funciona bien independientemente del número de lecturas.
-     * Usa el ParameterizedTest para realizar un número de lecturas previas a calcular si hay apnea o no (por ejemplo 4, 5 y 10 lecturas).
-     * https://junit.org/junit5/docs/current/user-guide/index.html#writing-tests-parameterized-tests
-     */
+    @DisplayName("Si se realizan 5 lecturas con valores por encima de los umbrales, se considera que hay una apnea en proceso")
+    @Test
+    public void testEvaluarApneaSuenyo2() {
+        // STEP 1: create mock object
+        DispositivoSilver mockedDispositivo = mock(DispositivoSilver.class);
+
+        // STEP 2: stubbing
+        when(mockedDispositivo.leerSensorPresion()).thenReturn(20.0f);
+        when(mockedDispositivo.leerSensorSonido()).thenReturn(30.0f);
+
+        // STEP 3: using the mocked object
+        RonQI2Silver ronQi2Silver = new RonQI2Silver();
+        ronQi2Silver.anyadirDispositivo(mockedDispositivo);
+        ronQi2Silver.obtenerNuevaLectura();
+        ronQi2Silver.obtenerNuevaLectura();
+        ronQi2Silver.obtenerNuevaLectura();
+        ronQi2Silver.obtenerNuevaLectura();
+        ronQi2Silver.obtenerNuevaLectura();
+        boolean result = ronQi2Silver.evaluarApneaSuenyo();
+
+        // STEP 4: asserting
+        assertTrue(result);
+    }
+
+    
+
+    
+    
+    
 }
