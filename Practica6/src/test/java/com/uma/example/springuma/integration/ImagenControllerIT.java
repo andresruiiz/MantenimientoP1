e385833182c3aa3dc4143610cef7f726176d340c
andresruiiz <andresruiz@uma.es>
2024-05-14 10:56:30 +0200

    .

--- Practica6/src/test/java/com/uma/example/springuma/integration/ImagenControllerIT.java
+++ Practica6/src/test/java/com/uma/example/springuma/integration/ImagenControllerIT.java
@@ -1,21 +1,28 @@
 package com.uma.example.springuma.integration;
 
+import java.io.File;
 import java.time.Duration;
 import java.util.Calendar;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.test.web.reactive.server.FluxExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
 
 import com.uma.example.springuma.integration.base.AbstractIntegration;
 import com.uma.example.springuma.model.Imagen;
 import com.uma.example.springuma.model.Medico;
 import com.uma.example.springuma.model.Paciente;
 
-import io.swagger.v3.oas.models.media.MediaType;
+import org.springframework.http.MediaType;
 import jakarta.annotation.PostConstruct;
 import reactor.core.publisher.Mono;
 
@@ -57,7 +64,29 @@ class ImagenControllerWebTestClientIT {
         imagen.setId(1);
         imagen.setNombre("Mamografía");
         imagen.setFecha(Calendar.getInstance());
+        imagen.setPaciente(paciente);
 
+    }
 
+    @Test
+    @DisplayName("Crear imagen")
+    public void crearPersonaPost_seObtieneConGet() {
+
+        File uploadFile = new File("./src/test/resources/healthy.png");
+
+        MultipartBodyBuilder builder = new MultipartBodyBuilder();
+        builder.part("image", new FileSystemResource(uploadFile));
+
+        //Crea una imagen
+        FluxExchangeResult<String> responseBody = client.post()
+            .uri("/imagen")
+            .contentType(MediaType.MULTIPART_FORM_DATA)
+            .body(BodyInserters.fromMultipartData(builder.build()))
+            .exchange()
+            .expectStatus().is2xxSuccessful().returnResult(String.class);
+
+        String result = responseBody.getResponseBody().blockFirst();
+        
+        assertEquals("{\"response\" : \"file uploaded successfully : healthy.png \"}", result);
     }
 }
