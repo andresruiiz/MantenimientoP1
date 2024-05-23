88ba146df98a14ac7692f111082dec151d01a0a5
Mel-Nicolo <nicolomelley@uma.es>
2024-05-23 13:08:40 +0200

    .

--- Practica8/Scripts/CrearInformeTest.js
+++ Practica8/Scripts/CrearInformeTest.js
@@ -25,14 +25,34 @@ export default async function () {
 
     page.locator('input[name="nombre"]').type('Luis');
     page.locator('input[name="DNI"]').type('123123');
-
+    
     const submitButton = page.locator('button[name="login"]');
+
     await Promise.all([page.waitForNavigation({waitUntil: 'networkidle'}), submitButton.click()]);
+    sleep(2);
 
-    const selectPacienteButton = page.$$("table tbody tr")[0];
-    await Promise.all([page.waitForNavigation({waitUntil: 'networkidle'}), selectPacienteButton.click()]);
+    const pacienteRow = page.$$("table tbody tr")[0];
+    await Promise.all([page.waitForNavigation({waitUntil: 'networkidle'}), pacienteRow.click()]);
 
-    page.waitForSelector('table tbody')
+    page.waitForSelector('table tbody');
+    sleep(2);
+
+    const viewButton = page.$$("table tbody tr")[0].$('button[name="view"]');
+    await Promise.all([page.waitForNavigation({waitUntil: 'networkidle'}), viewButton.click()]);
+
+    const informeButton = page.locator('button[title="Añadir informe"]');
+    await Promise.all([page.waitForNavigation({waitUntil: 'networkidle'}), informeButton.click()]);
+
+    sleep(5);
+    page.locator('textarea.mat-mdc-input-element.mdc-text-field__input').type('Informe de prueba');
+    sleep(10);
+
+    const saveButton = page.locator('button[name="save"]');
+    await Promise.all([page.waitForNavigation({waitUntil: 'networkidle'}), saveButton.click()]);
+
+    check(page, {
+  'informe': p => p.locator('span.info-value').textContent().includes('Informe de prueba'),
+});
 
   } finally {
     page.close();
